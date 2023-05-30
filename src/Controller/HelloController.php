029ba4fd7b92dee3b21287b157d401eb930f8557
tomasz-nyckowiak <tomasz.nyckowiak.programista@gmail.com>
2023-05-30 10:29:15 +0200

    Symfony 101

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController extends AbstractController 
+{
+    private array $messages = [
+        ['message' => 'Hello', 'created' => '2023/01/12'],
+        ['message' => 'Hi', 'created' => '2022/06/12'],
+        ['message' => 'Bye!', 'created' => '2021/05/12']
+    ];
+    
+    #[Route('/{limit?3}', name: 'app_index')]
+    public function index(int $limit = null): Response 
+    {        
+        return $this->render(
+            'hello/index.html.twig',
+            [
+                'messages' => $this->messages,
+                'limit' => $limit
+            ]
+            );
+    }
+
+    #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
+    public function showOne(int $id): Response
+    {
+        return $this->render(
+            'hello/show_one.html.twig',
+            [
+                'message' => $this->messages[$id]
+            ]
+            );
+        
+        //return new Response($this->messages[$id]);
+    }
+}
