fa3ec0dcbc6256423643a2f29d33804def0cb269
tomasz-nyckowiak <tomasz.nyckowiak.programista@gmail.com>
2023-07-24 11:47:54 +0200

    Section 4 (Forms) completed

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\MicroPost;
+use App\Form\MicroPostType;
 use App\Repository\MicroPostRepository;
 use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -25,5 +28,58 @@ class MicroPostController extends AbstractController
         return $this->render('micro_post/show.html.twig', [
             'post' => $post,
         ]);
+    }
+
+
+    #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
+    public function add(Request $request, MicroPostRepository $posts): Response 
+    {
+        $form = $this->createForm(MicroPostType::class, new MicroPost());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $post->setCreated(new DateTime());
+            $posts->save($post, true);
+
+            //1. Add a flash method - message being displayed to the person that submitted the form was successful
+            //2. And then this message disappears on further requests
+            $this->addFlash('success', 'Your micro post have been added');
+
+            return $this->redirectToRoute('app_micro_post');
+            //3. Redirect to some different page
+
+        }
+        
+        return $this->render(
+            'micro_post/add.html.twig',
+            [
+                'form' => $form
+            ]
+        );
+    }
+
+    #[Route('/micro-post/{post}/edit', name: 'app_micro_post_edit')]
+    public function edit(MicroPost $post, Request $request, MicroPostRepository $posts): Response 
+    {
+        $form = $this->createForm(MicroPostType::class, $post);        
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $posts->save($post, true);
+            
+            // Add a flash
+            $this->addFlash('success', 'Your micro post have been updated');
+
+            return $this->redirectToRoute('app_micro_post');
+        }
+        
+        return $this->render(
+            'micro_post/add.html.twig',
+            [
+                'form' => $form
+            ]
+        );
     }
 }
