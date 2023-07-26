574759fedc5a27f4fd7a1062218537b208262488
tomasz-nyckowiak <tomasz.nyckowiak.programista@gmail.com>
2023-07-26 12:27:43 +0200

    Section 6 completed

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,6 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Entity\MicroPost;
+use App\Entity\User;
+use App\Entity\UserProfile;
+use App\Repository\CommentRepository;
+use App\Repository\MicroPostRepository;
+use App\Repository\UserProfileRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,14 +22,47 @@ class HelloController extends AbstractController
         ['message' => 'Bye!', 'created' => '2021/05/12']
     ];
     
-    #[Route('/{limit<\d+>?3}', name: 'app_index')]
-    public function index(int $limit = null): Response 
+    #[Route('/', name: 'app_index')]
+    public function index(MicroPostRepository $posts, CommentRepository $comments): Response 
     {        
+        /*$post = new MicroPost();
+        $post->setTitle('Hello');
+        $post->setText('Hello');
+        $post->setCreated(new DateTime());*/
+
+        /*$post = $posts->find(14);
+        $comment = $post->getComments()[0];
+        $comment->setPost(null);
+        $comments->save($comment, true);*/
+        
+        //$post->removeComment($comment);
+        //$posts->save($post, true);
+
+        // dd($post);
+
+        /*$comment = new Comment();
+        $comment->setText('Hello');
+        $comment->setPost($post);
+        // $post->addComment($comment);
+        // $posts->save($post, true);
+        $comments->save($comment, true);*/
+        
+        /*$user = new User();
+        $user->setEmail('email@email.com');
+        $user->setPassword('12345678');
+        
+        $profile = new UserProfile();
+        $profile->setUser($user);
+        $profiles->save($profile, true);*/
+
+        // $profile = $profiles->find(1);
+        // $profiles->remove($profile, true);
+        
         return $this->render(
             'hello/index.html.twig',
             [
                 'messages' => $this->messages,
-                'limit' => $limit
+                'limit' => 3
             ]
             );
     }
